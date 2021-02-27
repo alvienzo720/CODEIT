9db47d3786c2b80846739e5f14d3d3f569f5b4cf
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-02-27 12:36:54 +0300

    Intital commit from alvin mutebi

---
+++ firday_26.py
@@ -0,0 +1,11 @@
+def add():
+    num1 = int(input("Please enter your first number: "))
+
+    num2 = int(input("Please enter your second number: "))
+
+    sum = num1 + num2
+
+    print(sum)
+
+
+add()
