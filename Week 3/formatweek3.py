cdf8e03d20a8883a199c34adf1dc13c0de9f1b5a
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-09 19:32:31 +0300

    Week Three day 1

---
+++ Week 3/formatweek3.py
@@ -0,0 +1,14 @@
+name = "My name is {name}, I'm {age} years old"
+
+f1 = name.format(name="Mike", age=21)
+
+print(f1)
+
+name = input("Enter a Name: ")
+
+age = input("Enter age: ")
+
+f2 = f"My name is {name}, I'm {age} years old"
+
+print(f2)
+
