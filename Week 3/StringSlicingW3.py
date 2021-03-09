9ee68f72bf604ea4e8674e5cfd78971c3ecae02d
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-09 19:46:39 +0300

    Week Three day 1

---
+++ Week 3/StringSlicingW3.py
@@ -0,0 +1,20 @@
+# string indexing
+
+day = "How was your day?"
+
+print(day[0])  # first index is always Zero
+
+print(len(day))
+
+print(day[16])
+
+print(day[-1])  # the last Indexs
+
+print(day[:])
+
+print(day[3:])  # stars from the third index and prints each other word
+
+print(day[:10])  # here we consider (n-1) prints from zero index upto 9th index
+
+print(day[4:10])  # prints from 4th index included to 10-1 upper bound(9th index)
+
