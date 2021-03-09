62c1052a652965d364749cc242ff22ecd3c16409
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-09 20:10:53 +0300

    Week Three day 1

--- Week 3/StringSlicingW3.py
+++ Week 3/StringSlicingW3.py
@@ -17,4 +17,31 @@ print(day[3:])  # stars from the third index and prints each other word
 print(day[:10])  # here we consider (n-1) prints from zero index upto 9th index
 
 print(day[4:10])  # prints from 4th index included to 10-1 upper bound(9th index)
+'''
+def replace(s,t,n):
+    # string -s, target -t, new -n
+        
+    return
+
+replace(x,y,z)
+'''
+# challenge
+question = "How's your night so far?"
+
+question = question[4:] + question[:]
+
+print(question)
+
+
+def str_replace(x, y, z):
+    x = input("Input enter a sentence")
+    y = input("Please enter a word to replace word")
+    z = input("Please enter a target")
+    for i in range(x):
+        if x[i] == y:
+            y = x[i]
+            return y
+
+
+str_replace("hello boys", "boys", "girls")
 
