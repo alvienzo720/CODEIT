5835421bccc004a4313c7a1a2cfab9fae6fdb98f
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-11 07:59:56 +0300

    Thursday assignment

--- Week 3/loops_w3.py
+++ Week 3/loops_w3.py
@@ -19,5 +19,24 @@ if num % 2 == 0:
 else:
     print("num is not an even integer")
 
+cities = [['Kampala', 75.0], ['Newark', 30.0], ['Amsterdam', 50.0],
+          ['Gulu', 80.0], ['Younde', 90.0], ['HongKong', 80.0],
+          ['San Francicso', 60.0], ['Busan', 60.0]]
 
+# 1 . print out all the items within the list
+for city in cities:
+    for one_city in city:
+        print(type(one_city))
+        print(one_city)
+
+print("-----------------------------------------")
+# 2. convert the temp from fareni to Celcius print the list
+for city in cities:
+    print(f"{city[0]} {city[1] - 32 * 5/9}")
+
+# 3 add a second index value to each inner list(temp in Kelvins
+print("-------------------------------------------")
+for city in cities:
+    city.append("kelvins")
+    print(city)
 
