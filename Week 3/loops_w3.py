e58e6179ea911c0919a8545b24465b426e34c6bf
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-11 06:15:32 +0300

    Thursday Week 3

--- Week 3/loops_w3.py
+++ Week 3/loops_w3.py
@@ -7,3 +7,17 @@ for letter in day:
     print(day.index(letter))
 
 # nesting
+
+num = 10
+
+if num % 2 == 0:
+    print("Even")
+    if num % 5 == 0:
+        print("Multiple of 5")
+    else:
+        print("even but not a multiple of 5")
+else:
+    print("num is not an even integer")
+
+
+
