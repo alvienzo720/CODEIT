ab8e1231924af80ab195ccd31ab09ffb2349511c
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-12 20:01:30 +0300

    Friday Week 3 Notes

--- Week 3/while_loopsWeek#.py
+++ Week 3/while_loopsWeek#.py
@@ -2,11 +2,22 @@
    # print("hello World")
 # while loops only run when a condition is true
 count = 0
+# takes a word
+# take a number
 
-while count <= 10:
-    print(count)
-    count = count + 3
+#word = input("Enter a word")
+num = 2
+# while count <= 10:
+#     print(count)
+#     count = count + 3
 
 # the least amount of time a while loop can run is zero
 
 # the least amount of time a loop can run is 1
+# word = input("Enter a word")
+
+num = 2
+
+while num <= count:
+    print(num)
+    num += 1
