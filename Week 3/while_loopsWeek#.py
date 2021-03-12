0045362427c4a51821c3ea2fc5680fc9c676bae5
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-12 19:40:17 +0300

    Friday Week 3 Notes

---
+++ Week 3/while_loopsWeek#.py
@@ -0,0 +1,12 @@
+#swhile True:
+   # print("hello World")
+# while loops only run when a condition is true
+count = 0
+
+while count <= 10:
+    print(count)
+    count = count + 3
+
+# the least amount of time a while loop can run is zero
+
+# the least amount of time a loop can run is 1
