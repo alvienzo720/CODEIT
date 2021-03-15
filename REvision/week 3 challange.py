6be44652e9bdc9698e8700e2182d3ef232fd6e52
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-15 18:29:14 +0300

    Week 4

---
+++ REvision/week 3 challange.py
@@ -0,0 +1,36 @@
+# Week 3 challenge:
+
+# Week 3 challenge:
+
+star = "*"
+
+# for i in range(5):
+#     #print(star)
+#     star = star+"*" # concatenation
+
+#
+
+
+# using while loop
+# count = 0
+# while count < 5:
+#     print("Hello" * count)
+#     count += 1
+
+alpha = ['a', 'b', 'c']
+num = 0
+# for i in alpha:
+#     #print(i)
+#     for m in i:
+#         print(i + m)
+
+for i in range(len(alpha)):
+    for k in range(len(alpha)):
+        print(alpha[i] + alpha[k])
+
+print("-----------------------------")
+count = 0
+while count < 3:
+    while count < 3:
+        print(f"{alpha}")
+        count += 1
