a0e0f65d1da085c577b3821797dad59303a51b31
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-15 19:35:14 +0300

    Week 4

--- Week 4/tuplesWeek4.py
+++ Week 4/tuplesWeek4.py
@@ -1,9 +1,31 @@
 # array:Tuples
+# they are inmutable
+a_tup = (1, "Hello", 5.6, False)
+b_tup = 'goodbye', True
 
-tup = (1, "Hello", 5.6, False)
+print(type(a_tup))
 
-print(type(tup))
-
-tup_list = list(tup)
-
+# converting tuple to list
+tup_list = list(a_tup)
+print(type(tup_list))
 print(tup_list)
+
+# concatinating tuples
+
+print(a_tup + b_tup)
+
+# singleton tuple
+sing_tup = ("single",)
+
+
+# slicing tuples
+
+print(a_tup[2:])
+
+# removing tuples
+first_item = a_tup
+remove_hello = first_item + a_tup[2:]
+print(remove_hello)
+
+
+
