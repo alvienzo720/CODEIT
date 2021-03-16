4a5e34e90ded1cb0cc829b1df86a5f6a85327a63
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-16 05:05:23 +0300

    Friday Week 4 Notes

--- Week 4/escapeSquences.py
+++ Week 4/escapeSquences.py
@@ -17,6 +17,7 @@ print("It\'s rainy outside \n")
 
 tup = ([1, 'a'], [2, 'b'], [3, 'c'], [4, 'd'], [5, 'e'])
 
+# O(n)
 for m in tup:
     if 'a' in m:
         print("Found a")
@@ -24,3 +25,20 @@ for m in tup:
     else:
         print("Could not find a")
 
+# note in
+
+# Tuple Challenge Day 3 Week $
+
+tup = ([1, 'a'], [2, 'b'], [4, 'd'], [5, 'e'])
+
+
+# add [3, 'c'] at index 2
+def addTuple():
+    tup_a = tup[:2]
+    tup_c = tup[2:]
+    tup_b = ([3, 'c'],)
+    final = tup_a + tup_b + tup_c
+    print(final)
+
+
+addTuple()
