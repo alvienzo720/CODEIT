3880be69c1fd4a83ee3cfba4ddfd30946044c20a
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-17 06:58:22 +0300

    Week 4 Notes

---
+++ Week 4/scope_sets_challange.py
@@ -0,0 +1,21 @@
+# 1.define two functions outer an inner
+
+'''
+
+nest inner function insode the outer function
+
+'''
+
+# 2. define a local variable "x" in the outer function
+
+# 3. define a global variable "x" in the inner function
+
+
+def outer():
+    x = "Hello"
+
+    def inner():
+        global x
+
+
+
