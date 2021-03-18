3335cb21ad568ede75a12f25316285947c6a6f44
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-18 07:58:44 +0300

    Week 4 Notes

--- Week 4/dictionaries.py
+++ Week 4/dictionaries.py
@@ -4,8 +4,17 @@
 Mapping types with Key, Value pairs
 '''
 
+# dictionaries with strings as keys and integers as keys
 
 city_weather = {'Jinja': 80, 'Newark': 30}
 
 print(type(city_weather))
 print(city_weather)
+print(city_weather['Jinja'])
+
+flower_color = dict([('daisy',['white','blue']), ('sunflower',{'yellow', 'brown'})])
+print(flower_color['daisy'][1])
+
+print(flower_color['sunflower'])
+
+info = {'City':['Kampala', 'New York City'], 'Ages':(79, 80, 15)}
