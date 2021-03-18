4a028f19bf8a4885a578430c32c0788fd417341f
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-18 07:19:34 +0300

    Week 4 Notes

---
+++ Week 4/dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionaries :Python hash tables
+
+'''
+Mapping types with Key, Value pairs
+'''
+
+
+city_weather = {'Jinja': 80, 'Newark': 30}
+
+print(type(city_weather))
+print(city_weather)
