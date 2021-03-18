cf92e3b0f3275a3caf1f11093b8ddb6b7861bc4f
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-18 05:01:52 +0300

    Week 4 Notes

---
+++ Week 4/scopSetchallange2.py
@@ -0,0 +1,27 @@
+# 1: Define a set with the following values
+
+'''
+
+1,2,3,'a','b','c'
+
+'''
+
+# 2: define function called global_set
+
+# 3: remove all numbers from the set
+
+'''
+'a','b','c'
+
+'''
+
+# 4: add a three new letter to the set
+
+'''
+'a','b','c','d','e','f'
+
+'''
+
+# 5: return the new set inside the function
+
+# 6: call global_set and pass the set defined as a parameter
