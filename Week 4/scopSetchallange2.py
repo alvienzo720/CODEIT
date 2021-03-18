98666e4a668be1b76bebb0b12432a4bf824dbe61
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-18 05:19:19 +0300

    Week 4 Notes

--- Week 4/scopSetchallange2.py
+++ Week 4/scopSetchallange2.py
@@ -25,3 +25,22 @@
 # 5: return the new set inside the function
 
 # 6: call global_set and pass the set defined as a parameter
+
+# 7: Print the set variable underneath the function call
+
+a = {1, 2, 3, 'a', 'b', 'c'}
+
+
+def global_set(m):
+    m.remove(1)
+    m.remove(2)
+    m.remove(3)
+    m.add("d")
+    m.add("e")
+    m.add("f")
+    print(m)
+    return m
+
+
+global_set(a)
+print(a)
