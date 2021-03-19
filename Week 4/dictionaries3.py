2925c192d9f9a626930f98d68137376c41176917
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-19 07:05:11 +0300

    Friday Notes

--- Week 4/dictionaries3.py
+++ Week 4/dictionaries3.py
@@ -1,6 +1,6 @@
-employees = {'id':[201, 600],
-             'name':["Maximo", "Josephine"],
-             'bonus':[1000, 2000]}
+employees = {'id': [201, 600],
+             'name': ["Maximo", "Josephine"],
+             'bonus': [1000, 2000]}
 
 print(employees)
 
@@ -12,10 +12,17 @@ print(m)
 print(f"{employees['id']} {employees['name']} {employees['bonus']}")
 
 
-# 2: print out all list values iterartively now
+# 2: print out all list values iteratively now
 
-for key ,value in employees.items():
+for key, value in employees.items():
     print(key, value)
 
-# 3: add "locations" to the employees dictionary location:('USA', 'Uganda')
-print(f"{employees.update('location':{'USA', 'Uganda'})
+# 3: add "locations" to the employees dictionary location:('USA', 'Uganda
+employees.update({'location': ('USA', 'Uganda')})
+print(employees)
+
+# 4: Update the dictionary with new employee
+# id -100
+# name -"Hughey"
+# bonus - 500
+# location
