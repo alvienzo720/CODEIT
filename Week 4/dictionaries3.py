507144f9d6da3a64d80e66c35223c441b263a0be
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-19 04:42:20 +0300

    Friday Notes

--- Week 4/dictionaries3.py
+++ Week 4/dictionaries3.py
@@ -6,6 +6,15 @@ print(employees)
 
 # 1: print out all the values of the dictionaries
 
-print(employees.values())
+m = employees.values()
+print(m)
 
 print(f"{employees['id']} {employees['name']} {employees['bonus']}")
+
+
+# 2: print out all list values iterartively now
+
+for word in m:
+    print(word)
+
+# 3:
