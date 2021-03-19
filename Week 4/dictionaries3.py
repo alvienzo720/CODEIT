56d3b24f3a64af1dd47e612360e84cf744e739b3
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-19 04:30:16 +0300

    Friday Notes

---
+++ Week 4/dictionaries3.py
@@ -0,0 +1,11 @@
+employees = {'id':[201, 600],
+             'name':["Maximo", "Josephine"],
+             'bonus':[1000, 2000]}
+
+print(employees)
+
+# 1: print out all the values of the dictionaries
+
+print(employees.values())
+
+print(f"{employees['id']} {employees['name']} {employees['bonus']}")
