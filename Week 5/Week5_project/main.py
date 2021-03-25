3a6a52a0d47d2c1cb115b7fd4620b6dc1c133163
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-03-25 08:35:25 +0300

    prohect files for weather app

--- Week 5/Week5_project/main.py
+++ Week 5/Week5_project/main.py
@@ -13,3 +13,16 @@ base.rowconfigure(0, minsize=50)
 base.columnconfigure([0, 1, 2], minsize=2)
 
 
+'''start our fields and labels which will be displaying
+the data'''
+# title labels
+title_1 = Label(text="Weather App", width=20, font=("bold", 20), bg="#9bd7e8")
+
+title_2 = Label(text="Search for Weather of any city of your choice", width=32, font=("italics", 15), bg="#9bd7e8")
+# Search feild and button in grid format
+
+search_city = tk.Entry(text="Search for city")
+button_search = tk.Button(text="Search", bg="white")
+
+# temp output and label
+
