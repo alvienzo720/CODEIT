5d1791c4fae30d5e088505b195d23dc02128fbb1
alvienzo720 <mutebialvinalvienzo@gmail.com>
2021-05-05 07:19:28 +0300

    more about Numpy

--- Week_2_Joseph/wednesday.py
+++ Week_2_Joseph/wednesday.py
@@ -31,3 +31,48 @@ print("cit =")
 print(cit)
 print("cit_3 =")
 print(cit_3)
+
+# using fromiter function
+
+mr_Hue_report = "cit studnets are good to go for thier friday presentation"
+
+response = np.fromiter(mr_Hue_report, dtype="U2")
+print(response)
+
+# we are to demostrate how full() function is used
+
+cit_full = np.full([3, 5], 55)
+print(cit_full)
+
+list1 = [2,4,7,8,9]
+list2 = [4,5,6,7,8]
+
+vector1 = np.array(list1)
+vector2 = np.array(list2)
+
+addition = vector1 + vector2
+
+print("additional vector : "+ str(addition))
+sub = vector1 + vector2
+print("Subtraction vector:" + str(sub))
+
+multi = vector1 * vector2
+print("Multiplication vector :" + str(multi))
+
+# how to use fromrecords() function
+# syntax format
+# numpy.core.fromrecoreds([(tuple1),(tuple2)], metadata)
+# returns a records array
+
+cit_record = np.core.records.fromrecords([(1, 'Dashone',27),(2, 'Elijah',34)], names ='Joseph,Name,Age')
+print(cit_record[:1])
+
+# using view() function in numpy
+
+Array = np.array([1,2,3,4,4,5,5,9])
+#creating the view
+V1 = Array.view()
+print("identity of array", id(Array))
+print("identity of array",id(V1))
+Array[3] = 4
+# printinting array and view
